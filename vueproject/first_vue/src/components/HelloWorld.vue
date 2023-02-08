cbef427580fa1d0a6241b50072181e353af6e9ec
geonxee <1913kg@naver.com>
2023-02-09 07:56:29 +0900

    feat: Vue Practice

---
+++ vueproject/first_vue/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>나니아 구역</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+  </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
